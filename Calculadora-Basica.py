154a5453ac0860e12cefa2285b0730fa754dae99
Mkbrox <156731405+Mkbrox@users.noreply.github.com>
2024-01-24 08:57:42 -0500

    Create Calculadora-Basica.py

---
+++ Calculadora-Basica.py
@@ -0,0 +1,33 @@
+#Calculadora Basica
+'''Oscar Gabriel Lasso Inca'''
+
+'''Description: 
+La calculadora realiza las operaciones matematicas 
+basicas'''
+
+#Variables (2 variables estáticas)
+num1 = 15
+num2 = 20
+
+#PROCESO
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#SALIDAS
+print("::: BASIC CALC V1.0 :::")
+print("Number 1: ", num1, type(num1))
+print("Number 2: ", num2, type(num2))
+print("The addition is: ", add, type(add))
+print("The subtraction is: ", sub, type(sub))
+print("The multiplication is: ", mul, type(mul))
+print("The division is: ", div, type(div))
+print("\n")
+print("::: BASIC CALC V1.1 :::")
+print("Number 1: ", num1)
+print("Number 2: ", num2)
+print("The addition is: ", num1+num2)
+print("The subtraction is: ", num1-num2)
+print("The multiplication is: ", num1*num2)
+print("The division is: ", num1/num2)
